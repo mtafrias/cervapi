49591a0bc9f7570d2e41c21e21ef76a9e6061aae
mtafrias <mtafrias@gmail.com>
2021-03-22 01:33:14 -0300

    Help, please.

--- src/main/java/br/com/brewers/cervapi/config/SecurityConfig.java
+++ src/main/java/br/com/brewers/cervapi/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.brewers.cervapi.config;
 import br.com.brewers.cervapi.controllers.UserController;
 import br.com.brewers.cervapi.models.Role;
 import br.com.brewers.cervapi.services.user.UserServiceImpl;
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,6 +14,11 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -26,7 +32,23 @@ public class SecurityConfig {
                 .pathMatchers(HttpMethod.POST).hasAuthority(Role.Tipo.EDITOR.getName())
                 .pathMatchers(HttpMethod.DELETE).hasAuthority(Role.Tipo.EDITOR.getName())
                 .pathMatchers(HttpMethod.PUT).hasAuthority(Role.Tipo.EDITOR.getName())
+                .and().cors().configurationSource(getConfigurationSource())
                 .and().httpBasic().and().build();
+    }
+
+    private CorsConfigurationSource getConfigurationSource() {
+        val source = new UrlBasedCorsConfigurationSource();
+        val configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.addAllowedOrigin("https://cervapi-website.herokuapp.com");
+        val all = Collections.singletonList("*");
+        configuration.setAllowedMethods(all);
+        configuration.setAllowedHeaders(all);
+        configuration.setExposedHeaders(all);
+        configuration.setMaxAge(3600L);
+        configuration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
